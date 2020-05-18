4ce939de40fdd73d3daa1602a58397e6f1585b09
Johannes Schneider <jojo20872@gmail.com>
2020-05-17 22:54:52 -0400

    Remove files now in .gitignore

--- Course_Data_Scraping/path_config.py
+++
@@ -1,2 +0,0 @@
-DRIVER_PATH = "C:\\Users\\jojo2\\Drivers\\Chrome\\chromedriver_win32\\chromedriver.exe"
-COURSE_DATA_PATH = "C:\\Users\\jojo2\\Documents\\PythonProjects\\MC-Schedule-Builder\\Course_Data"
