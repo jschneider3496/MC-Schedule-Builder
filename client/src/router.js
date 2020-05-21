9837c61e1fe8251f298c7857fe6edce460f4e927
Johannes Schneider <jojo20872@gmail.com>
2020-05-21 18:08:16 -0400

    mdbootstrap

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from './components/Ping.vue';
+import Builder from './components/Builder.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/ping',
       name: 'Ping',
       component: Ping,
+    },
+    {
+      path: '/builder',
+      name: 'Builder',
+      component: Builder,
     },
   ],
 });
