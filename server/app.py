91a26a75c257683db9728823e45ca58e0d06f1b3
Johannes Schneider <jojo20872@gmail.com>
2020-05-21 17:52:26 -0400

    Update

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
+import json
 
 
 # configuration
@@ -12,12 +14,45 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+SCHEDULE = []
+id = ""
+name = ""
+with open('../schedules/b82fe23e366a464aae0798c40b67ec53.json') as infile:
+    temp = json.load(infile)
+    id = temp["id"]
+    name = temp["name"]
+    SCHEDULE = temp["schedule"]
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/builder', methods=['GET'])
+def builder_menu():
+    return jsonify({
+        'status': 'success',
+        'schedule': SCHEDULE,
+        'name': name,
+        'id': id
+    })
+
+# @app.route('/builder/<builder_id>', methods = ['GET', 'POST'])
+# def builder_menu():
+#     response_object = {'status': 'success'}
+#     if request.method == 'POST':
+#         post_data = request.get_json()
+#         BOOKS.append({
+#             'id': uuid.uuid4().hex,
+#             'title': post_data.get('title'),
+#             'author': post_data.get('author'),
+#             'read': post_data.get('read')
+#         })
+#         write_json(BOOKS)
+#         response_object['message'] = 'Book added!'
+#     else:
+#         response_object['books'] = BOOKS
+#     return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
